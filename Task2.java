381d68b8ee1a4075cf43989cfa7e036b2df0120d
John Louie Malinao <cleartech0510@gmail.com>
2025-08-28 04:39:43 +0000

    Task2 Done

---
+++ Task2.java
@@ -0,0 +1,18 @@
+public class Task2 {
+  public static void main(String args[]) {
+    char h = 'H';
+    byte three = 3;
+    byte one = 1;
+    byte one1 = 1;
+    short zero = 0;
+    short zero1 = 0;
+    char w = 'w';
+    char r ='r';
+    int one3 = 1;
+    char d = 'd';
+    float two = 2;
+    boolean t = true;
+    String output = ""+ h + three + one + one1 + zero + " " + w +zero1 + r +one3+d +" "+two+" "+t;
+    System.out.println(output);
+  }
+}
