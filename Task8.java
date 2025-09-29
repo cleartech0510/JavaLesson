4ac206c5fabf3fb7028adde48ca8391a26e94845
John Louie Malinao <cleartech0510@gmail.com>
2025-09-29 04:13:03 +0000

    task 8 done

---
+++ Task8.java
@@ -0,0 +1,20 @@
+public class Task8 {
+
+    public static int calculateCumulativeSum(int... numbers) {
+        int totalSum = 0;
+
+        System.out.println("Cumulative sums for each parameter:");
+        for (int num : numbers) {
+            int cumulative = (num * (num + 1)) / 2; // Using arithmetic formula
+            System.out.println("Sum of numbers from 1 to " + num + " = " + cumulative);
+            totalSum += cumulative;
+        }
+
+        return totalSum;
+    }
+
+    public static void main(String[] args) {
+        int result = calculateCumulativeSum(4, 5, 10);
+        System.out.println("Total sum of all cumulative sums = " + result);
+    }
+}
