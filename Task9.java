9df0b37eeb67aa2e5439a5ac8ae3a47c38e32322
John Louie Malinao <cleartech0510@gmail.com>
2025-09-29 04:40:49 +0000

    Task9

---
+++ Task9.java
@@ -0,0 +1,37 @@
+import static java.lang.Math.*;
+import java.util.Scanner;
+
+public class Task9 {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static float divide(int a, int b) {
+        return (float) a / b;
+    }
+
+    public static void main(String[] args) {
+        
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter 1st Number: ");
+        int Dig1 = s.nextInt();
+        System.out.println("Enter 2nd Number: ");
+        int Dig2 = s.nextInt();
+        
+        System.out.println("Math Operations Results:");
+        System.out.println("Numbers: " + Dig1 + " and " + Dig2);
+        System.out.println("Addition: " + add(Dig1, Dig2));
+        System.out.println("Subtraction: " + subtract(Dig1, Dig2));
+        System.out.println("Multiplication: " + multiply(Dig1, Dig2));
+        System.out.println("Division: " + divide(Dig1, Dig2));
+    }
+}
